3aac69faa5bb0b541ac87a8618ebb8e8ac26d74e
enrycoop <enricocoppolecchia@outlook.com>
2020-01-02 16:31:10 +0100

    aggiornato

--- KfoldCrossValidation.py
+++ KfoldCrossValidation.py
@@ -1,14 +1,16 @@
 from data_manager import *
 from data_access import *
-
+import arff
+import subprocess
 
 if __name__ == '__main__':
+    '''
     X = DatabaseWrapper().getAllTransactions()
     sep = ','
     date = X[0][1]
     id = X[0][0]
     lines = []
-    for x in X[:1000]:
+    for x in X:
         if x[0] == id:
             days = timeDiffinDays(x[1],date)
             date = x[1]
@@ -17,6 +19,8 @@ if __name__ == '__main__':
             id = x[0]
             date = x[1]
             x[1] = 0
-        lines.append(f"{x[0]},{x[1]},{x[2]},'{x[3]}','{x[4]}','{x[5]}'\n")
-    with open('trans.csv','w') as f:
-        f.writelines(lines)
+        lines.append(f"{x[0]},{x[1]},{x[2]},{x[3]},'{x[4]}','{x[5]}','{x[6]}'\n")
+
+    arff.dump('trans.arff',X,relation='transactions',names=['account_id','timediff','amount','balance','k_symbol','operation','type'])'''
+    subprocess.run(['java', '-jar', '-Xmx4048m', 'external_libraries/clusSSL.jar', '-xval',
+                    f'external_libraries/conf.s'])
--- external_libraries/conf.s
+++ external_libraries/conf.s
@@ -1,24 +1,23 @@
 [General]
-RandomSeed = 1
 Verbose = 0
 
 [Data]
-File = resources/9/train5000.arff
-TestSet = resources/9/test5000.arff
+File = trans2.arff
 
 [Attributes]
-Target = 3
-Descriptive = 2,4-7
+Target = 5
+Descriptive = 2-4,6,7
+Disable = 1
+Weights = Normalize
 
 [Tree]
-Heuristic = VarianceReduction
+Heuristic = GainRatio
 
 [Ensemble]
 Iterations = 100
 EnsembleMethod = RForest
 
 [SemiSupervised]
-UnlabeledData = resources/9/unlabeled5000.arff
 SemiSupervisedMethod = PCT
 InternalFolds = 3
 
